10e8c37d29d6bccfce80d9b91369d07b5128c914
willjasen <willjasen@gmail.com>
2015-05-30 14:21:37 -0700

    creating track method, errors with undefined local variable or method hostname

--- app/models/ted_reader.rb
+++ app/models/ted_reader.rb
@@ -1,3 +1,23 @@
 class TedReader < ActiveRecord::Base
   validates_presence_of :hostname
+
+  def self.track
+    enum = Enumerator.new do |yielder|
+      url = "http://#{hostname}/api/LiveData.xml"
+      user = ""
+      password = ""
+      client = HTTPClient.new
+      client.set_auth(url, user, password)
+      client.get(url) do |chunk|
+        yielder << chunk
+      end
+    end
+
+    document = Oga.parse_xml(enum)
+    document.xpath('LiveData/Voltage/MTU1').each do |mtu1|
+      @current_voltage = mtu1.at_xpath('VoltageNow').text
+      puts @current_voltage
+    end
+  end
+
 end
