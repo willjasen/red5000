dced99bd2fdd13d1323983738985c9719a347f71
Brandon <willjasen@gmail.com>
2015-11-11 00:16:59 -0500

    adding recurrent logging

--- config/recurrences.rb
+++ config/recurrences.rb
@@ -1,5 +1,9 @@
 configure.logging = "quiet"
 
+configure.logger do |message, log_level|
+  Rails.logger.info.send(log_level, message)
+end
+
 every 5.seconds, :save => true do
   TedReader.track
 end
