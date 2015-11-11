cf671d7aa7769d0aa6afb5663d873a031e2def09
Brandon <willjasen@gmail.com>
2015-11-11 16:54:08 -0500

    changing Reader to TedReader

--- db/migrate/20150808205405_add_ted_reader_ref_to_readings.rb
+++ db/migrate/20150808205405_add_ted_reader_ref_to_readings.rb
@@ -1,4 +1,4 @@
-class AddReaderRefToReadings < ActiveRecord::Migration
+class AddTedReaderRefToReadings < ActiveRecord::Migration
   def change
     add_reference :readings, :ted_reader, index: true, foreign_key: true
   end
