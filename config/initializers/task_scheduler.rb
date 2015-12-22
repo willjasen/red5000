5a10e2d176c0a3d61a58d50687f4f06eea763572
Brandon <willjasen@gmail.com>
2015-12-22 10:50:46 -0500

    track each TedReader

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,6 +1,8 @@
 Rails.application.config.after_initialize do
   scheduler = Rufus::Scheduler.new
   scheduler.every '5s' do
-    TedReader.track
+    TedReader.find_each do |ted_reader|
+      ted_reader.track
+    end
   end
 end
